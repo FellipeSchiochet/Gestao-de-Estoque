639a839a59b1f235f4ab448c21bf6c5ec507acd0
Fellipe Schiochet <fellipeschiochet090204@gmail.com>
2026-03-27 08:41:30 -0300

    Fix install script for older dotnet installer

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 DOTNET_INSTALL_DIR="$(pwd)/.dotnet"
 
 mkdir -p "$DOTNET_INSTALL_DIR"
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0 --install-dir "$DOTNET_INSTALL_DIR" --skip-path
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0 --install-dir "$DOTNET_INSTALL_DIR"
 
 echo "DOTNET_INSTALL_DIR=$DOTNET_INSTALL_DIR"
